50a446775ffa37e3d45a385bfbcf1dd0c3e6b996
ebbiesimon <114156365+ebbiesimon@users.noreply.github.com>
2023-07-17 17:57:06 +0100

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,8 @@
+def calculate_bmi():
+    height = float(input("Enter your height in meters: "))
+    weight = float(input("Enter your weight in kilograms: "))
+
+    bmi = weight / (height ** 2)
+    print(f"Your BMI is {bmi:.2f}")
+
+calculate_bmi()
